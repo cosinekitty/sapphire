29fc87b999bd9ff4357b2c739d16062c160720c0
Don Cross <cosinekitty@gmail.com>
2022-06-20 21:22:25 -0400

    Use spaces, not tabs. Use my own coding style.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -4,12 +4,13 @@
 Plugin* pluginInstance;
 
 
-void init(Plugin* p) {
-	pluginInstance = p;
+void init(Plugin* p)
+{
+    pluginInstance = p;
 
-	// Add modules here
-	// p->addModel(modelMyModule);
+    // Add modules here
+    // p->addModel(modelMyModule);
 
-	// Any other plugin initialization may go here.
-	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+    // Any other plugin initialization may go here.
+    // As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
 }
