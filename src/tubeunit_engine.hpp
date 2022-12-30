0783495333393145ce1991b8fdfb14897b6a7ed9
Don Cross <cosinekitty@gmail.com>
2022-12-29 21:32:34 -0500

    Tube Unit: more work toward first sound synthesis.

--- src/tubeunit_engine.hpp
+++ src/tubeunit_engine.hpp
@@ -17,10 +17,15 @@ namespace Sapphire
         DelayLine<float> outbound;
         DelayLine<float> inbound;
         float sampleRate = 0.0f;
-        float rootFrequency = 0.0f;
-        float formantFrequency = 0.0f;
-        bool dirty = true;      // Do the delay lines need to be reconfigured?
-        bool active = true;     // Is the voice actively making sound?
+        float rootFrequency;
+        float formantFrequency;
+        bool dirty;                 // Do the delay lines need to be reconfigured?
+        bool active;                // Is the voice actively making sound?
+        float voiceOnThreshold;     // the pressure below which we trigger a voice burst
+        float voicePressure;
+        int burstCounter;
+        int burstDirection;
+        int burstCycle = 0;
 
         void configure()
         {
@@ -30,13 +35,15 @@ namespace Sapphire
             if (rootFrequency <= 0.0f)
                 throw std::logic_error("Invalid root frequency in TubeUnitEngine");
 
+            if (formantFrequency <= 0.0f)
+                throw std::logic_error("Invalid formant frequency in TubeUnitEngine");
+
             // FIXFIXFIX: Implement fractional sample wavelength using sinc formula.
             // For now, round down to integer number of samples.
             // Divide wavelength by 2 because we have both inbound and outbound delay lines.
             // But we can still get to the nearest integer sample by allowing one delay
             // line to have an odd length and the other an even length, if needed.
             // FIXFIXFIX ??? Do we really need two delay lines, or can we merge into one delay line?
-            // Consider a single delay line, but expanded to support multiple "taps".
 
             size_t nsamples = static_cast<size_t>(std::floor(sampleRate / rootFrequency));
             size_t smallerHalf = nsamples / 2;
@@ -45,6 +52,9 @@ namespace Sapphire
             // The `setLength` calls will clamp the delay line lengths as needed.
             outbound.setLength(smallerHalf);
             inbound.setLength(largerHalf);
+
+            // The burst cycle length is the number of samples we send pressure for when the voice unit is active.
+            burstCycle = std::max(1, static_cast<int>(std::floor(sampleRate / formantFrequency)));
         }
 
     public:
@@ -57,6 +67,12 @@ namespace Sapphire
         {
             inbound.clear();
             outbound.clear();
+            rootFrequency = TubeUnitDefaultRootFrequencyHz;
+            formantFrequency = TubeUnitDefaultFormantFrequencyHz;
+            voiceOnThreshold = 0.1f;
+            voicePressure = 0.0f;
+            burstCounter = 0;
+            burstDirection = 0;
             dirty = true;       // force re-configure
             active = true;      // voice is active by default
         }
@@ -101,18 +117,55 @@ namespace Sapphire
                 dirty = false;
             }
 
+            // The tube has two ends: an open end where the sound comes out
+            // and a closed end where the "voice" is applied. The so-called
+            // voice is analogous to a clarinet reed or a trumpet player's lips.
             // Find the effective air pressure at the closed end of the tube.
             float inSignal = inbound.readForward(0) + outbound.readBackward(0);
-            (void) inSignal;
+
+            // Find the effective pressure the open end of the tube.
+            float outSignal = inbound.readBackward(0) + outbound.readForward(0);
+
+            voicePressure = 0.0f;
 
             if (active)
             {
                 // The voice gate is high. Keep the formant vibrating.
+                // When the pressure is below a certain threshold, trigger a burst of higher pressure.
+                if (inSignal < voiceOnThreshold && burstDirection == 0)
+                {
+                    burstCounter = 0;
+                    burstDirection = +1;
+                }
+
+                if (burstDirection != 0)
+                {
+                    burstCounter += burstDirection;
+                    if (burstDirection > 0)
+                    {
+                        if (burstCounter >= burstCycle)
+                            burstDirection = -1;    // Start moving the opposite way
+                    }
+                    else if (burstDirection < 0)
+                    {
+                        if (burstCounter <= 0)
+                            burstDirection = 0;     // Come to a halt
+                    }
+
+                    voicePressure = static_cast<float>(burstCounter) / static_cast<float>(burstCycle);
+                }
             }
+            else
+            {
+                burstDirection = 0;
+            }
+
+            float reflectionPressure = 0.5 * outSignal;    // FIXFIXFIX - not a real formula
 
             // Even if the voice gate is low (not active), there can be a fadeout period.
             // Keep virbrations moving through the two waveguides (delay lines).
-
+            outbound.write(voicePressure);
+            inbound.write(reflectionPressure);
         }
     };
 }
