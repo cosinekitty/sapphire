5a7c72ea0ad6e2cf3caa31f1018c7276383a19f7
Don Cross <cosinekitty@gmail.com>
2023-04-16 16:45:25 -0400

    Blank SVG panel design for Cantilever.

---
+++ util/cantilever_svg.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+#
+#   cantilever_svg.py   -   Don Cross <cosinekitty@gmail.com>
+#
+#   Generates SVG panel and widget positions for Cantilever module.
+#
+import sys
+from typing import Tuple
+from svgpanel import *
+from sapphire import *
+
+PANEL_WIDTH = 12
+
+def Print(message:str) -> int:
+    print('tubeunit_svg.py:', message)
+    return 0
+
+
+def Save(panel:Panel, filename:str) -> int:
+    panel.save(filename)
+    return Print('Wrote: ' + filename)
+
+
+def GenerateMainPanel() -> int:
+    svgFileName = '../res/cantilever.svg'
+    panel = Panel(PANEL_WIDTH)
+    pl = Element('g', 'PanelLayer')
+    pl.append(BorderRect(PANEL_WIDTH, SAPPHIRE_PANEL_COLOR, SAPPHIRE_BORDER_COLOR))
+    with Font(SAPPHIRE_FONT_FILENAME) as font:
+        pl.append(SapphireInsignia(panel, font))
+        pl.append(ModelNamePath(panel, font, 'cantilever'))
+    panel.append(pl)
+    return Save(panel, svgFileName)
+
+
+if __name__ == '__main__':
+    sys.exit(
+        GenerateMainPanel() or
+        Print('SUCCESS')
+    )
