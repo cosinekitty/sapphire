97f795532d61084344db533a10f4944d552868a8
Don Cross <cosinekitty@gmail.com>
2023-10-19 16:27:15 -0400

    Frolic: added labels to panel: OUT, X, Y, Z.

--- util/chaos_svg.py
+++ util/chaos_svg.py
@@ -36,6 +36,14 @@ def GenerateChaosPanel(name: str) -> int:
         pl.append(Component('z_output', xmid, outputPortY1 + 2*outputPortDY))
         pl.append(Component('speed_knob', xmid, 30.0))
         pl.append(Component('chaos_knob', xmid, 60.0))
+        xOutLabel = xmid - 3.9
+        yOutLabel = outputPortY1 - 10.0
+        pl.append(ControlTextPath(font, 'OUT', xOutLabel, yOutLabel, 'out_label'))
+        xPortLabel = xmid - 7.5
+        yPortLabel = outputPortY1 - 2.4
+        pl.append(ControlTextPath(font, 'X',  xPortLabel, yPortLabel + 0*outputPortDY, 'port_label_x'))
+        pl.append(ControlTextPath(font, 'Y',  xPortLabel, yPortLabel + 1*outputPortDY, 'port_label_y'))
+        pl.append(ControlTextPath(font, 'Z',  xPortLabel, yPortLabel + 2*outputPortDY, 'port_label_z'))
     panel.append(pl)
     return Save(panel, svgFileName)
 
