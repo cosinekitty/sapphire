18494142ab539348eb40bc82d2189206799b5463
Don Cross <cosinekitty@gmail.com>
2024-02-11 21:32:48 -0500

    Nucleus standalone: added better comments
    The standalone test will be a helpful reference
    for anyone who wants to adapt Nucleus to other platforms.
    Therefore, it's good to have some basic starter
    comments in this standalone test, to help new people
    get oriented.

--- util/cmdline/nucleus_standalone.cpp
+++ util/cmdline/nucleus_standalone.cpp
@@ -15,11 +15,11 @@ int main()
     using namespace std;
     using namespace Sapphire;
 
-    const int SAMPLE_RATE = 44100;
-    const int CHANNELS = 2;
+    const int SAMPLE_RATE = 44100;      // CD sampling rate
+    const int CHANNELS = 2;             // stereo output
     const int DURATION_SECONDS = 10;
     const int DURATION_SAMPLES = SAMPLE_RATE * DURATION_SECONDS;
-    const int SETTLE_SECONDS = 1;
+    const int SETTLE_SECONDS = 1;       // how many seconds to allow initial settling of the Nucleus engine
     const int SETTLE_SAMPLES = SAMPLE_RATE * SETTLE_SECONDS;
 
     NucleusEngine engine{5};
@@ -54,25 +54,33 @@ int main()
         }
     }
 
-    // Apply a pulse from 500 ms to 510 ms.
+    // Apply a square wave pulse as input over the following range of samples.
     const int pulseStartSample  = SampleFromSeconds(0.500, SAMPLE_RATE);
     const int pulseFinishSample = SampleFromSeconds(1.000, SAMPLE_RATE);
 
     float sample[CHANNELS];
     for (int s = 0; s < DURATION_SAMPLES; ++s)
     {
+        // Set the position of particle A as input.
         Particle& input = engine.particle(0);
         input.pos[0] = (s >= pulseStartSample && s <= pulseFinishSample) ? 10 * inputDrive : 0;
         input.pos[1] = 0;
         input.pos[2] = 0;
         input.pos[3] = 0;
+        // Also make sure particle A's velocity is always zero.
         input.vel = PhysicsVector::zero();
+
+        // Run the simulation for one sample.
         engine.update(speed/SAMPLE_RATE, halflife, SAMPLE_RATE, 1.0f);
+
+        // Look for any non-finite numbers in the simulation.
         if (crashChecker.check(engine))
         {
             fprintf(stderr, "ERROR: Nucleus engine produced non-finite output at sample %d (rendering).\n", s);
             return 1;
         }
+
+        // Use (BX, BY) outputs as stereo (left, right) for the output WAV file.
         sample[0] = engine.output(1, 0);    // BX
         sample[1] = engine.output(1, 1);    // BY
         wave.WriteSamples(sample, CHANNELS);
