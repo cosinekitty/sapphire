c272ff8017d64ffa02f20321374c892985d444d3
Don Cross <cosinekitty@gmail.com>
2024-04-08 05:56:40 -0400

    Slew: updated for process() returning particle state.

--- src/slew.cpp
+++ src/slew.cpp
@@ -94,8 +94,8 @@ namespace Sapphire
                     float speed = cvGetControlValue(SPEED_KNOB_PARAM, SPEED_ATTEN_PARAM, cvSpeed, MinSpeed, MaxSpeed);
                     float viscosity = cvGetControlValue(VISCOSITY_KNOB_PARAM, VISCOSITY_ATTEN_PARAM, cvViscosity, MinViscosity, MaxViscosity);
                     float dt = args.sampleTime * std::pow(2.0f, speed);
-                    float vSlewOutput = engine[c].process(dt, vTarget, viscosity);
-                    outputs[SLEW_OUTPUT].setVoltage(vSlewOutput, c);
+                    ParticleState particle = engine[c].process(dt, vTarget, viscosity);
+                    outputs[SLEW_OUTPUT].setVoltage(particle.pos, c);
                 }
             }
         };
