5ec431825e0ea4b56b11c756b6867940ef144c30
Don Cross <cosinekitty@gmail.com>
2025-01-19 15:56:14 -0500

    Elastika panel: final connector lines added, but not in export svg.

--- util/make_sapphire_svg.py
+++ util/make_sapphire_svg.py
@@ -1394,10 +1394,18 @@ def GenerateElastikaPanel(cdict:Dict[str, ControlLayer], svgFileName:str, hasAtt
             ky = 102.0      # vertical position of both knobs
             kdx = 6.5
             kdy = 13.0
+            qx1 = kx1 - 0.1
+            qx2 = kx2 - 0.1
+            qdx = 6.6       # horizontal offset of text labels "L", "R"
+            qy = 109.3
             pl.append(GeneralLine(kx1, ky, kx1-kdx, ky+kdy, 'left_input_line'))
             pl.append(GeneralLine(kx1, ky, kx1+kdx, ky+kdy, 'right_input_line'))
             pl.append(GeneralLine(kx2, ky, kx2-kdx, ky+kdy, 'left_output_line'))
             pl.append(GeneralLine(kx2, ky, kx2+kdx, ky+kdy, 'right_output_line'))
+            pl.append(CenteredControlTextPath(font, 'L', qx1-qdx, qy))
+            pl.append(CenteredControlTextPath(font, 'R', qx1+qdx, qy))
+            pl.append(CenteredControlTextPath(font, 'L', qx2-qdx, qy))
+            pl.append(CenteredControlTextPath(font, 'R', qx2+qdx, qy))
 
         # IN/OUT labels for TILT knobs...
         pl.append(CenteredControlTextPath(font, 'IN',   tx1, 26.0))
