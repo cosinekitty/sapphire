d2ea4baeee37c43287aac507ae832a9e08c18638
Don Cross <cosinekitty@gmail.com>
2025-08-15 14:09:09 -0400

    Zoo: starting to create a preset validator/cleaner.

---
+++ util/clean_sapphire_presets.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+#
+#   clean_sapphire_presets.py
+#   Don Cross <cosinekitty@gmail.com>
+#   https://github.com/cosinekitty/sapphire
+#
+#   Validates and cleans factory presets for Sapphire Zoo.
+#
+import sys
+import os
+import json
+from typing import List
+
+def Print(message:str) -> int:
+    print('clean_sapphire_presets.py:', message)
+    return 0
+
+
+def UpdateFileIfChanged(filename:str, newText:str) -> bool:
+    # Do not write to the file unless the newText is different from
+    # what already exists in the file, or we can't even read text from the file.
+    # This allows us to change the modification date on a file only
+    # when something has really changed. This way, we don't trick
+    # `make` into doing unnecessary work
+    # (like compiling C++ code that hasn't changed).
+    try:
+        with open(filename, 'rt') as infile:
+            oldText = infile.read()
+    except:
+        oldText = ''
+    if newText == oldText:
+        Print('Kept:  {}'.format(filename))
+        return False
+    with open(filename, 'wt') as outfile:
+        outfile.write(newText)
+    Print('Wrote: {}'.format(filename))
+    return True
+
+
+def FactoryPresets(moduleSlug:str) -> List[str]:
+    path = os.path.join('../presets', moduleSlug)
+    fnlist:List[str] = []
+    for fn in os.listdir(path):
+        if fn.endswith('.vcvm'):
+            fullname = os.path.join(path, fn)
+            fnlist.append(fullname)
+    return fnlist
+
+
+def CleanZooPreset(fn:str) -> int:
+    with open(fn, 'rt') as infile:
+        oldText = infile.read()
+    preset = json.loads(oldText)
+    newText = json.dumps(preset, indent=2)
+    if newText == oldText:
+        Print('Kept:  {}'.format(fn))
+    else:
+        with open(fn, 'wt') as outfile:
+            outfile.write(newText)
+        Print('Wrote: {}'.format(fn))
+    return 0
+
+
+def CleanZooPresets() -> int:
+    # Iterate through all Zoo vcvm files.
+    for fn in FactoryPresets('Zoo'):
+        if CleanZooPreset(fn): return 1
+    return Print('SUCCESS')
+
+if __name__ == '__main__':
+    sys.exit(CleanZooPresets())
